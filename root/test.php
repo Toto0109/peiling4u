4a745278c177d46965e731045f71a7db814e6125
Toto0109 <Toto0109@users.noreply.github.com>
2019-11-23 14:12:43 +0100

    switch filenames(iets logischer)

--- root/test.php
+++ root/test.php
@@ -1,30 +1,45 @@
 <?php
 include "connect.php";
+//include "test.php";
 session_start();
 
-function hey()
-{
-    echo "hey";
-}
-
-function get_usrname($gebruikersnr)
+function get_usrname(int $gebruikersnr)
 {
     $mysql = mysqli_connect($server,$user,$pass,$db) 
         or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
-echo $gebruikersnr;
-$gebruiker = mysqli_real_escape_string($mysql, $gebruikersnr);
-echo $gebruiker;
+    
+    echo $gebruikersnr;
+    $gebruiker = mysqli_real_escape_string($mysql, $gebruikersnr);
+    echo $gebruiker;
     
     $resultaat = mysqli_query($mysql,"SELECT gebruikersnaam
                                       FROM gebruikers 
-                                      WHERE gebruikersnr = 8")//"'$gebruiker'") 
+                                      WHERE gebruikersnr = '$gebruikersnr'") 
         or die("De query 1 op de database is mislukt!");
-    
+     
     mysqli_close($mysql) 
         or die("Het verbreken van de verbinding met de MySQL-server is mislukt!");
 
-    list($gebruikersnaam) = mysqli_fetch_row($resultaat);
-    
+    //list($gebruikersnaam) = mysqli_fetch_row($resultaat);
     return $gebruikersnaam;
 }
+
+$asdf = $_SESSION["gebruiker"];
+echo get_usrname($asdf);
+//hey();
+/*
+$mysql = mysqli_connect($server,$user,$pass,$db) 
+        or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
+$asdf = $_SESSION["gebruiker"];    
+$resultaat = mysqli_query($mysql,"SELECT gebruikersnaam
+                                  FROM gebruikers 
+                                  WHERE gebruikersnr = '$asdf'")
+    or die("De query 1 op de database is mislukt!");
+    
+mysqli_close($mysql) 
+    or die("Het verbreken van de verbinding met de MySQL-server is mislukt!");
+
+list($gebruikersnaam) = mysqli_fetch_row($resultaat);
+
+echo $gebruikersnaam;*/
 ?>
