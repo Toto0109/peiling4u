bb6fd73dbeae2007e10243acaeae4ba3b8d0ecbe
weeb69 <anthony1sep@gmail.com>
2019-11-26 10:59:11 +0100

    list vragen in creator.php

--- root/functies.php
+++ root/functies.php
@@ -1,6 +1,59 @@
 <?php
 include "connect.php";
 session_start();
+/*
+function count_rows($tabel)
+{
+    global $server, $user, $pass, $db;
+    $mysql = mysqli_connect($server,$user,$pass,$db) 
+        or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
+    
+    $resultaat = mysqli_query($mysql,"SELECT COUNT(*)
+                                      FROM '$tabel'") 
+        or die("De query 1 op de database is mislukt!");
+    
+    mysqli_close($mysql) 
+        or die("Het verbreken van de verbinding met de MySQL-server is mislukt!");
+
+    list($count) = mysqli_fetch_row($resultaat);
+    return $count;
+}
+*/
+function count_vragen($peilingnr)
+{
+    global $server, $user, $pass, $db;
+    $mysql = mysqli_connect($server,$user,$pass,$db) 
+        or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
+    
+    $resultaat = mysqli_query($mysql,"SELECT COUNT(*)
+                                      FROM vragen
+                                      WHERE peilingnr = '$peilingnr'") 
+        or die("De query 1 op de database is mislukt!");
+    
+    mysqli_close($mysql) 
+        or die("Het verbreken van de verbinding met de MySQL-server is mislukt!");
+
+    list($count) = mysqli_fetch_row($resultaat);
+    return $count;
+}
+
+function count_antwoorden($peilingnr, $vraagnr)
+{
+    global $server, $user, $pass, $db;
+    $mysql = mysqli_connect($server,$user,$pass,$db) 
+        or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
+    
+    $resultaat = mysqli_query($mysql,"SELECT COUNT(*)
+                                      FROM antwoorden
+                                      WHERE peilingnr = '$peilingnr' AND vraagnr = '$vraagnr'") 
+        or die("De query 1 op de database is mislukt!");
+    
+    mysqli_close($mysql) 
+        or die("Het verbreken van de verbinding met de MySQL-server is mislukt!");
+
+    list($count) = mysqli_fetch_row($resultaat);
+    return $count;
+}   
 
 function get_gebruikersnaam($gebruikersnr)
 {
@@ -62,10 +115,37 @@ function get_gebruikersnr($peilingnr)
     return $gebruikersnr;
 }
 
+function get_vraag($vraagnr)
+{
+    global $server, $user, $pass, $db;
+    $mysql = mysqli_connect($server,$user,$pass,$db) 
+        or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
+    
+    $resultaat = mysqli_query($mysql,"SELECT vraag
+                                      FROM vragen
+                                      WHERE vraagnr = '$vraagnr'") 
+        or die("De query 1 op de database is mislukt!");
+     
+    mysqli_close($mysql) 
+        or die("Het verbreken van de verbinding met de MySQL-server is mislukt!");
+
+    list($vraag) = mysqli_fetch_row($resultaat);
+    return $vraag;
+}
+
 function bewerk_peiling($peilingnr)
 {
     echo "<form action='creator.php?nr='$peilingnr' method='post'>";
-    echo "<input type='text' name='gebruikersnaam'/> <br>";
+    for ($i = 1; $i <= count_vragen($peilingnr); $i++)
+    {
+        echo "Vraag ".$i.": ";
+        $vraag = get_vraag($i);
+        echo "<input type='text' name='vraag' value='$vraag'><br>";
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <input type='submit' name='add_antwoord' value='+'><br>";
+    }
+    echo "<input type='submit' name='add_vraag' value='+'> <br>";
+    echo "<input type='submit' name='save' value='Sla de peiling op'> <br>";
     echo "</form>";
 }
 ?>
